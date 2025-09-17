8afe645540966fc63e83b340f09b42d8882de395
jonathan-queiroz <jonathanqueiroz94@gmail.com>
2025-09-17 10:17:23 -0300

    Corrigindo lint

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,8 +10,8 @@ module Types
       context.schema.object_from_id(id, context)
     end
 
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
+    field :nodes, [ Types::NodeType, null: true ], null: true, description: "Fetches a list of objects given a list of IDs." do
+      argument :ids, [ ID ], required: true, description: "IDs of the objects."
     end
 
     def nodes(ids:)
@@ -21,7 +21,7 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :news, [Types::NewsType], null: false, description: "Lista todas as notícias" do
+    field :news, [ Types::NewsType ], null: false, description: "Lista todas as notícias" do
       # Filtros ou paginação podem ser adicionados futuramente
     end
 
